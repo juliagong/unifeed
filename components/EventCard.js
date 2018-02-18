d2ef748f12fa49370411ee3ea8b7b30881dd437c
Mac Bagwell <macbagwell@hprb-rtr.stanford.edu>
2018-02-17 18:32:38 -0800

    Added tags, author, button shadows, improved margins

--- components/EventCard.js
+++ components/EventCard.js
@@ -4,7 +4,7 @@ import { Text,
          ScrollView,
          StyleSheet,
          Dimensions } from 'react-native';
-import { Card, Tile, Button, ButtonGroup } from 'react-native-elements';
+import { Card, Tile, Button, ButtonGroup, Divider } from 'react-native-elements'
 import Expo from 'expo';
 
 export class EventCard extends React.Component {
@@ -13,36 +13,60 @@ export class EventCard extends React.Component {
       <Card title={this.props.title}
             titleStyle={styles.title}
             style={styles.card} >
-        <View style={styles.info}>
-          <View style={{ alignItems: 'flex-start' }}>
-            <Text style={styles.location}>{this.props.loc}</Text>
-          </View>
-          <View style={{ alignItems: 'flex-end'}}>
-            <Text style={styles.author}>{"@" + this.props.time}</Text>
-          </View>
-        </View>
+            <View style={styles.info}>
+              <View>
+                <Text style={styles.location}>{this.props.loc}</Text>
+              </View>
+              <View>
+                <Text style={styles.time}>{"@" + this.props.time}</Text>
+              </View>
+            </View>
         <View style={styles.buttons}>
           <Button
-          title = "Pin"
-          titleStyle={styles.buttonTitle}
-          buttonStyle={styles.button1}
+            title = "Pin"
+            titleStyle={styles.buttonTitle}
+            buttonStyle={styles.button1}
           />
           <Button
-          title = "Go"
-          titleStyle={styles.buttonTitle}
-          buttonStyle={styles.button2}
+            title = "Go"
+            titleStyle={styles.buttonTitle}
+            buttonStyle={styles.button2}
           />
           <Button
-          title = "Share"
-          titleStyle={styles.buttonTitle}
-          buttonStyle={styles.button3}
-          containerStyle={{marginVertical: 10}}
+            title = "Share"
+            titleStyle={styles.buttonTitle}
+            buttonStyle={styles.button3}
+            containerStyle={{marginVertical: 10}}
           />
         </View>
+        <Divider style={styles.divider} />
         <ScrollView style={styles.description}>
-          <Text style={styles.descriptionText}>{"Descpitions!"}</Text>
+          <Text style={styles.descriptionText}>{this.props.desc}</Text>
         </ScrollView>
-        <View>
+        <View style={{flexDirection: 'row', marginTop: 20}}>
+          <ScrollView horizontal={true} style={{marginRight: 20}}>
+              <Button
+                title="s2"
+                titleStyle={styles.tagTitle}
+                buttonStyle={styles.tagButton}
+                containerStyle={styles.tagContainer}
+              />
+              <Button
+                title="food"
+                titleStyle={styles.tagTitle}
+                buttonStyle={styles.tagButton}
+                containerStyle={styles.tagContainer}
+              />
+              <Button
+                title="donuts"
+                titleStyle={styles.tagTitle}
+                buttonStyle={styles.tagButton}
+                containerStyle={styles.tagContainer}
+              />
+          </ScrollView>
+          <View style={{ alignItems: 'flex-end' }}>
+            <Text style={styles.author}>"Stephone"</Text>
+          </View>
         </View>
       </Card>
     );
@@ -50,31 +74,34 @@ export class EventCard extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  card: {
-    backgroundColor: '#303036'
-  },
   description: {
     height: 150,
   },
   descriptionText: {
-    color: 'black'
+    color: 'white'
   },
   title: {
     textAlign: "left",
-    color: 'black'
+    color: 'white'
   },
   info: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
+    marginBottom: -50,
   },
   location: {
     textAlign: 'left',
-    color: 'black'
+    color: 'white',
+    height: 20
   },
-  author: {
+  time: {
     textAlign: 'right',
-    color: 'black'
+    color: 'white',
+    height: 20
+  },
+  divider: {
+    marginVertical: 10,
   },
   buttons: {
     flexDirection: 'row',
@@ -92,7 +119,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#99ccff',
     borderWidth: 0,
     borderColor: 'transparent',
-    borderRadius: 20
+    borderRadius: 20,
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.9,
+    shadowRadius: 2
   },
   button2: {
     height: 30,
@@ -100,7 +131,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#99cc00',
     borderWidth: 0,
     borderColor: 'transparent',
-    borderRadius: 20
+    borderRadius: 20,
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.9,
+    shadowRadius: 2
   },
   button3: {
     height: 30,
@@ -108,6 +143,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#ff9999',
     borderWidth: 0,
     borderColor: 'transparent',
-    borderRadius: 20
+    borderRadius: 20,
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.9,
+    shadowRadius: 2
   },
+  tagTitle: {
+    marginHorizontal: 5,
+    color: 'red'
+  },
+  tagButton: {
+    backgroundColor: '#909090',
+    width: null,
+    height: 20,
+    borderRadius: 3,
+    marginHorizontal: -10
+  },
+  tagContainer: {
+    marginTop: 20
+  },
+  author: {
+    color: 'white'
+  }
 });
