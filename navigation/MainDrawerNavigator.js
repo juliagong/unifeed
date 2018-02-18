c629c4f8870b6e18870cab88cc866c58cad390d7
Julia Gong <juliaxgong@gmail.com>
2018-02-17 17:40:38 -0800

    Added menu with headings and submenu items, changed color scheme

--- navigation/MainDrawerNavigator.js
+++ navigation/MainDrawerNavigator.js
@@ -1,15 +1,125 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import PropTypes from 'prop-types';
+import { Platform, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { DrawerNavigator} from 'react-navigation';
-
-import Colors from '../constants/Colors';
+import { DrawerNavigator } from 'react-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import FeedScreen from '../screens/FeedScreen';
 import MyEventsScreen from '../screens/MyEventsScreen';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+
+class SideMenu extends React.Component {
+  state = {
+    selectedIndex: null,
+    // data: [
+    //   {
+    //     name: 'My Feed',
+    //     children: [
+    //       { name: 'bacon', onPress:() => this.navigateToScreen('Home') }
+    //     ]
+    //   },
+    //   {
+    //     name: 'My Feed',
+    //     children: [
+    //       { name: 'bacon', onPress:() => this.navigateToScreen('Home') }
+    //     ]
+    //   }
+    // ]
+  }
+
+  navigateToScreen = (route) => () => {
+    const navigateAction = NavigationActions.navigate({
+      routeName: route
+    });
+    this.props.navigation.dispatch(navigateAction);
+  }
+
+  // renderChildren = (children) =>  children.map(this.renderItem)
+  //
+  //
+  // componentWillUpdate() {
+  //   LayoutAnimation.easeInEaseOut()
+  // }
+  //
+  // selectItem = (item) => {
+  //
+  // }
+  //
+  // renderItem = ({name, onPress, children, childrenVisible}, index) => (
+  //   <View>
+  //     <Text style={sideMenuStyles.navItemStyle} onPress={onPress || () => this.selectItem(name)}>
+  //     {name}
+  //     </Text>
+  //     {this.renderChildren(children)}
+  //   </View>
+  // )
+
+  render () {
+    return (
+      <View style={sideMenuStyles.container}>
+        <ScrollView>
+          <View>
+            <Text style={sideMenuStyles.sectionHeadingStyle} onPress={this.navigateToScreen('Feed')}>
+              My Feed
+            </Text>
+          </View>
+          <View>
+            <Text style={sideMenuStyles.sectionHeadingStyle} onPress={this.navigateToScreen('MyEvents')}>
+            My Events
+            </Text>
+          </View>
+          <View>
+            <Text style={sideMenuStyles.sectionHeadingStyle} onPress={this.navigateToScreen('Home')}>
+              My Groups
+            </Text>
+            <View style={sideMenuStyles.navSectionStyle}>
+              <Text style={sideMenuStyles.navItemStyle} onPress={this.navigateToScreen('Feed')}>
+                O-Tone
+              </Text>
+              <Text style={sideMenuStyles.navItemStyle} onPress={this.navigateToScreen('Feed')}>
+                SSI
+              </Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+SideMenu.propTypes = {
+  navigation: PropTypes.object
+};
+
+const sideMenuStyles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    flex: 1,
+    backgroundColor: '#34373d',
+  },
+  navItemStyle: {
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    color: 'white',
+  },
+  navSectionStyle: {
+    backgroundColor: '#202328',
+  },
+  sectionHeadingStyle: {
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    backgroundColor: '#34373d',
+    color: 'white',
+  },
+  footerContainer: {
+    padding: 20,
+    backgroundColor: '#202328',
+  },
+});
 
 export default DrawerNavigator(
   {
@@ -29,6 +139,10 @@ export default DrawerNavigator(
       screen: MyEventsScreen,
     },
   },
+  {
+    contentComponent: SideMenu,
+    drawerWidth: 300
+  }
   // {
   //   navigationOptions: ({ navigation }) => ({
   //
